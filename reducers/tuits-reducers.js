66b1f02e0dca75a84f182e302bbb6f3101ff9ad4
shreyamitra <smitra0504@gmail.com>
2022-04-09 02:47:05 -0400

    a8

--- reducers/tuits-reducers.js
+++ reducers/tuits-reducers.js
@@ -1,8 +1,8 @@
-import {FIND_ALL_TUITS}
-  from "/Users/shreyamitra/Desktop/2022/spring/full-stack-developer-server-node/actions/tuits-actions.js";
-const tuitsReducer = (state = [], action) => {
- switch (action.type) {
-   case FIND_ALL_TUITS:
-     return action.tuits;
- }
-}
+//import {FIND_ALL_TUITS}
+//  from "/Users/shreyamitra/Desktop/2022/spring/full-stack-developer-server-node/actions/tuits-actions.js";
+//const tuitsReducer = (state = [], action) => {
+// switch (action.type) {
+//   case FIND_ALL_TUITS:
+//     return action.tuits;
+// }
+//}
